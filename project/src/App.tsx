4c09db74a21648a781112309d231f2198b59a5ec
RehanAli7024 <bb1221388@iitd.ac.in>
2025-03-01 01:21:39 +0530

    Final Frontend added

---
+++ project/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AudioRecorder from './AudioRecorder'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-4">
+      <AudioRecorder />
+    </div>
+  )
+}
+
+export default App
